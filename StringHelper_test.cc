3a8e9fb3a98d99d550ebcdb0def2e6abb3682a13
Jaebin <anjaebin200701@gmail.com>
2025-05-13 20:32:27 +0900

    fix: long string generation add

--- StringHelper_test.cc
+++ StringHelper_test.cc
@@ -23,9 +23,11 @@ TEST(test, SpecialString) {
 }
 
 TEST(test, LongString) {
-    EXPECT_EQ(Utils::String::Larger::toUpperCase("AbCdEfGHiJkLmNoPqRs"), "ABCDEFGHIJKLMNOPQRS");
-    EXPECT_EQ(Utils::String::Smaller::toLowerCase("AbCdEfGHiJkLmNoPqRs"), "abcdefghijklmnopqrs");
-    EXPECT_EQ(Utils::String::ContainDetect::contains("AbCdEfGHiJkLmNoPqRs", "Ab"), 1);
-    EXPECT_EQ(Utils::String::ContainDetect::contains("AbCdEfGHiJkLmNoPqRs", "hello"), 0);
+    std::string small(1000, 'a');
+    std::string large(1000, 'A');
+    EXPECT_EQ(Utils::String::Larger::toUpperCase(small), large);
+    EXPECT_EQ(Utils::String::Smaller::toLowerCase(large), small);
+    EXPECT_EQ(Utils::String::ContainDetect::contains(small, "aaaaa"), 1);
+    EXPECT_EQ(Utils::String::ContainDetect::contains(small, "hello"), 0);
 }
 
